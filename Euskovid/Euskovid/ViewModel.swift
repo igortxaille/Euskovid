29f612a2ac0f732d0e0f1722c9f13d0185f477e9
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-02-25 11:50:54 +0100

    Carga de datos con responseString en vez de responseJSON

--- Euskovid/Euskovid/ViewModel.swift
+++ Euskovid/Euskovid/ViewModel.swift
@@ -43,18 +43,24 @@ class ViewModel: ObservableObject {
         let pcr = "https://opendata.euskadi.eus/contenidos/ds_informes_estudios/covid_19_2020/opendata/generated/covid19-pcr.json"
         
         
-        AF.request(pcr, method: .get).validate().responseJSON { response in
+        AF.request(pcr, method: .get).validate().responseString { response in
             switch response.result {
             case .success(let value):
-                let pcrJson = JSON(value)
 
-                let ultimo = pcrJson["byDate"].array!.last
-                
-                self.positivos = ultimo!["positiveCount"].stringValue
+                do {
+                    let dataFromString = value.data(using: .utf8, allowLossyConversion: false)
+                    let pcrJson = try JSON(data: dataFromString!)
+
+                    let ultimo = pcrJson["byDate"].array!.last
                     
-                self.incidencia = ultimo!["aggregatedIncidence"].stringValue
-                self.fecha = ultimo!["date"].stringValue
+                    self.positivos = ultimo!["positiveCount"].stringValue
+                        
+                    self.incidencia = ultimo!["aggregatedIncidence"].stringValue
+                    self.fecha = ultimo!["date"].stringValue
                 
+                } catch {
+                    print("Error al cargar el JSON")
+                }
 
             case .failure(let error):
                 print(error)
@@ -62,41 +68,51 @@ class ViewModel: ObservableObject {
         }
         let edad = "https://opendata.euskadi.eus/contenidos/ds_informes_estudios/covid_19_2020/opendata/generated/covid19-pcr-positives.json"
         
-        AF.request(edad, method: .get).validate().responseJSON { response in
+        AF.request(edad, method: .get).validate().responseString { response in
             switch response.result {
             case .success(let value):
-                let edadJson = JSON(value)
+                do {
+                    let dataFromString = value.data(using: .utf8, allowLossyConversion: false)
+                    let edadJson = try JSON(data: dataFromString!)
 
-                let ultimo = edadJson["byDate"].array!.last
-                
-                self.hombre = ultimo!["menCount"].stringValue
-                self.mujer = ultimo!["womenCount"].stringValue
-                self.hasta9 = ultimo!["age_0_9_Count"].stringValue
-                self.d10 = ultimo!["age_10_19_Count"].stringValue
-                self.d20 = ultimo!["age_20_29_Count"].stringValue
-                self.d30 = ultimo!["age_30_39_Count"].stringValue
-                self.d40 = ultimo!["age_40_49_Count"].stringValue
-                self.d50 = ultimo!["age_50_59_Count"].stringValue
-                self.d60 = ultimo!["age_60_69_Count"].stringValue
-                self.d70 = ultimo!["age_70_79_Count"].stringValue
-                self.d80 = ultimo!["age_80_89_Count"].stringValue
-                self.d90 = ultimo!["age_90_X_Count"].stringValue
-            case .failure(let error):
+                    let ultimo = edadJson["byDate"].array!.last
+                    
+                    self.hombre = ultimo!["menCount"].stringValue
+                    self.mujer = ultimo!["womenCount"].stringValue
+                    self.hasta9 = ultimo!["age_0_9_Count"].stringValue
+                    self.d10 = ultimo!["age_10_19_Count"].stringValue
+                    self.d20 = ultimo!["age_20_29_Count"].stringValue
+                    self.d30 = ultimo!["age_30_39_Count"].stringValue
+                    self.d40 = ultimo!["age_40_49_Count"].stringValue
+                    self.d50 = ultimo!["age_50_59_Count"].stringValue
+                    self.d60 = ultimo!["age_60_69_Count"].stringValue
+                    self.d70 = ultimo!["age_70_79_Count"].stringValue
+                    self.d80 = ultimo!["age_80_89_Count"].stringValue
+                    self.d90 = ultimo!["age_90_X_Count"].stringValue
+                } catch {
+                    print("Error al cargar el JSON")
+                }
+
+                    case .failure(let error):
                 print(error)
             }
         }
             let municipios = "https://opendata.euskadi.eus/contenidos/ds_informes_estudios/covid_19_2020/opendata/generated/covid19-bymunicipality.json"
-            AF.request(municipios, method: .get).validate().responseJSON { response in
+        AF.request(municipios, method: .get).validate().responseString { response in
                 switch response.result {
                 case .success(let value):
-                    let muniJson = JSON(value)
+                    do {
+                        let dataFromString = value.data(using: .utf8, allowLossyConversion: false)
+                        let muniJson = try JSON(data: dataFromString!)
 
-                    let ultimo = muniJson["date"].array!.last
-                    
-                    //self.fecha = ultimo!["date"].stringValue
-                    self.pueblo = ultimo!["officialName"].stringValue
-                    self.positCount = ultimo!["positiveCount"].stringValue
-                    
+                        let ultimo = muniJson["date"].array!.last
+                        
+                        //self.fecha = ultimo!["date"].stringValue
+                        self.pueblo = ultimo!["officialName"].stringValue
+                        self.positCount = ultimo!["positiveCount"].stringValue
+                    } catch {
+                        print("Error al cargar el JSON")
+                    }
                 
                 case .failure(let error):
                     print(error)
