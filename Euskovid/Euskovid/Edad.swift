373eb05efb5101f4c001fd9dc9fa56970a6e9eee
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2021-02-25 11:50:32 +0100

    Picker edad mejorado
    Creo que en realidad el problema era la carga del JSON

--- Euskovid/Euskovid/Edad.swift
+++ Euskovid/Euskovid/Edad.swift
@@ -6,7 +6,8 @@
 //
 
 import SwiftUI
-enum edad: String, CaseIterable, Identifiable {
+enum edad: CaseIterable, Identifiable, Hashable, CustomStringConvertible {
+
     case hasta9
     case de10a19
     case de20a29
@@ -17,155 +18,78 @@ enum edad: String, CaseIterable, Identifiable {
     case de70a79
     case de80a89
     case mas90
-    var id: String { self.rawValue }}
+
+    // Identifiable
+    var id: Self { self }
+
+    // CustomStringConvertible
+    // REF: https://martin.pizza/articles/posts/2020-05-30/reusable-picker-for-enums-in-swiftui.html
+    var description: String {
+        switch self {
+        case .hasta9: return "De 0 a 9 años"
+        case .de10a19: return "de 10 a 19 años"
+        case .de20a29: return "de 20 a 29 años"
+        case .de30a39: return "De 30 a 39 años"
+        case .de40a49: return "de 40 a 49 años"
+        case .de50a59: return "de 50 a 59 años"
+        case .de60a69: return "De 60 a 69 años"
+        case .de70a79: return "de 70 a 79 años"
+        case .de80a89: return "de 80 a 89 años"
+        case .mas90: return "más de 90"
+        }
+    }
+}
+
 struct Edad: View {
     @ObservedObject var vm = ViewModel()
     @State private var selectedEdad = edad.hasta9
     var body: some View {
-        
-        ZStack{
-        
-            HStack{
-                
+
+        VStack {
+
             Picker("edad", selection: $selectedEdad) {
-                Text("De 0 a 9 años").tag(edad.hasta9)
-                Text("de 10 a 19 años").tag(edad.de10a19)
-                Text("de 20 a 29 años").tag(edad.de20a29)
-                Text("De 30 a 39 años").tag(edad.de30a39)
-                Text("de 40 a 49 años").tag(edad.de40a49)
-                Text("de 50 a 59 años").tag(edad.de50a59)
-                Text("De 60 a 69 años").tag(edad.de60a69)
-                Text("de 70 a 79 años").tag(edad.de70a79)
-                Text("de 80 a 89 años").tag(edad .de80a89)
-                Text("mas de 90").tag(edad.mas90)
-                         }
-         
+                ForEach(Array(edad.allCases)) { value in
+                    Text(value.description).tag(value)
+                }
             }
-            
-            VStack{
-            
-                
-                if selectedEdad.rawValue == edad.hasta9.rawValue{
-                    pickerEdad()
-                }
-                if selectedEdad.rawValue == edad.de10a19.rawValue{
-                    pickerEdad()
-                        
-                        
-                }
-                if selectedEdad.rawValue == edad.de20a29.rawValue{
-                 pickerEdad()
-                        
-                       
-                }
-                if selectedEdad.rawValue == edad.de30a39.rawValue{
-                   pickerEdad()
-                        
-                        
-                }
-                if selectedEdad.rawValue == edad.de40a49.rawValue{
-                    pickerEdad()
-                        
-                        
-                }
-                if selectedEdad.rawValue == edad.de50a59.rawValue{
-                    pickerEdad()
-                        
-                        
-                }
-                if selectedEdad.rawValue == edad.de60a69.rawValue{
-                    pickerEdad()
-                    
-                       
-                }
-                if selectedEdad.rawValue == edad.de70a79.rawValue{
-                    pickerEdad()
-                        
-                        
-                }
-                if selectedEdad.rawValue == edad.de80a89.rawValue{
-                    pickerEdad()
-                       
-                        
-                }
-                if selectedEdad.rawValue == edad.mas90.rawValue{
-                    pickerEdad()
-                        
-                        
-                }
-                
-            }
-                   }
-        
+
+            pickerEdad()
+                .padding()
+
+        }
             .navigationBarTitle("Volver", displayMode: .inline)
-            .onAppear(){
-                vm.cargarPCR()
- 
+            .onAppear() {
+            vm.cargarPCR()
+
         }
-        
-        
+
+
     }
-    func pickerEdad() -> Text{
-        if selectedEdad.rawValue == edad.hasta9.rawValue{
-            return Text(vm.hasta9)
-               
-                
+
+    func pickerEdad() -> Text {
+
+        switch selectedEdad {
+        case .hasta9: return Text(vm.hasta9)
+        case .de10a19: return Text(vm.d10)
+        case .de20a29: return Text(vm.d20)
+        case .de30a39: return Text(vm.d30)
+        case .de40a49: return Text(vm.d40)
+        case .de50a59: return Text(vm.d50)
+        case .de60a69: return Text(vm.d60)
+        case .de70a79: return Text(vm.d70)
+        case .de80a89: return Text(vm.d80)
+        case .mas90: return Text(vm.d90)
         }
-        if selectedEdad.rawValue == edad.de10a19.rawValue{
-            return Text(vm.d10)
-                
-                
-        }
-        if selectedEdad.rawValue == edad.de20a29.rawValue{
-           return Text(vm.d20)
-                
-               
-        }
-        if selectedEdad.rawValue == edad.de30a39.rawValue{
-            return Text(vm.d30)
-                
-                
-        }
-        if selectedEdad.rawValue == edad.de40a49.rawValue{
-            return Text(vm.d40)
-                
-                
-        }
-        if selectedEdad.rawValue == edad.de50a59.rawValue{
-            return Text(vm.d50)
-                
-                
-        }
-        if selectedEdad.rawValue == edad.de60a69.rawValue{
-            return Text(vm.d60)
-            
-               
-        }
-        if selectedEdad.rawValue == edad.de70a79.rawValue{
-            return Text(vm.d70)
-                
-                
-        }
-        if selectedEdad.rawValue == edad.de80a89.rawValue{
-            return Text(vm.d80)
-               
-                
-        }
-        if selectedEdad.rawValue == edad.mas90.rawValue{
-            return Text(vm.d90)
-                
-                
-        }
-        return Text("Error")
+
     }
 }
 
 
 
-    struct Edad_Previews:
-        PreviewProvider {
-        static var previews: some View {
-            Edad()
+struct Edad_Previews:
+    PreviewProvider {
+    static var previews: some View {
+        Edad()
     }
 }
 
